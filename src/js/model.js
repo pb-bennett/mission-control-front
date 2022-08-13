af51d7b3f028f3e4e05e71e930bb1fbfb0253315
pb-bennett <pb.bennett.uk@gmail.com>
2022-08-13 11:18:56 +0200

    Starting to code a menu bar

--- src/js/model.js
+++ src/js/model.js
@@ -7,15 +7,10 @@ import { AJAX } from './helpers.js';
 
 export const loadMissions = async function (id = '') {
   try {
-<<<<<<< HEAD
-    const data = await AJAX(`http://84.234.239.112:3000/api/v1/missions/${id}`);
+    const data = await AJAX(`http://192.168.1.48:3000/api/v1/missions/${id}`);
     const missions = data.data.missions;
     if (Array.isArray(missions)) return missions;
     return [missions];
-=======
-    const data = await AJAX(`http://84.234.239.112:3000/api/v1/missions`);
-    return data.data;
->>>>>>> 84f29dfcfd80ca623fd6c44d77c9cce032ce2861
   } catch (err) {
     console.error(`${err} BOOOOOOM`);
     throw err;
